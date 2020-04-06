e734a20c58d968ea4be41fc0bca820abfed31c78
Anirudh Singh <anirudhsingh20.as@gmail.com>
2020-04-06 22:10:07 +0530

    first commit

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject platformPrefab;
+
+    public int numberOfPlatforms;
+    public float levelWidth = 3f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPos = new Vector3();
+        for( int i = 0; i< numberOfPlatforms; i++)
+        {
+            spawnPos.y += Random.Range(minY, maxY);
+            spawnPos.x = Random.Range(-levelWidth, +levelWidth);
+
+            Instantiate(platformPrefab, spawnPos, Quaternion.identity);
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    float movement;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        
+    }
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement * speed;
+        rb.velocity = velocity;
+    }
+}
